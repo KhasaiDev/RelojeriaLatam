2fd03985d03071532bef92dd5ae11e588f700981
KhasaiDev <khasaimassacre@gmail.com>
2024-07-18 22:33:56 -0400

    Very Stable Commit

--- app/src/main/java/com/example/relojerialatam/MainActivity.kt
+++ app/src/main/java/com/example/relojerialatam/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.relojerialatam
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -25,13 +28,85 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.toolbar)
         setContentView(binding.root)
         val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_Fragment) as NavHostFragment
         navController=navHostFragment.navController
         setupActionBarWithNavController(navController)
+        setupDestinationChangedListener()
+    }
+
+    private fun setupDestinationChangedListener() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.lobbyFragment -> {
+                    supportActionBar?.hide()
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                    supportActionBar?.setDisplayShowHomeEnabled(false)
+                }
+                R.id.sucursal1Fragment -> {
+                    supportActionBar?.title = "Cicero Store"
+                    supportActionBar?.show()
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    supportActionBar?.setDisplayShowHomeEnabled(true)
+                }
+                R.id.sucursal2Fragment -> {
+                    supportActionBar?.title = "Li Europan lingues"
+                    supportActionBar?.show()
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    supportActionBar?.setDisplayShowHomeEnabled(true)
+                }
+                R.id.sucursal3Fragment -> {
+                    supportActionBar?.title = "Kafka Store"
+                    supportActionBar?.show()
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    supportActionBar?.setDisplayShowHomeEnabled(true)
+                }
+                R.id.sucursal4Fragment -> {
+                    supportActionBar?.title = "Love Craft Arts"
+                    supportActionBar?.show()
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    supportActionBar?.setDisplayShowHomeEnabled(true)
+                }
+                R.id.sucursal5Fragment -> {
+                    supportActionBar?.title = "The Ancient Mariner"
+                    supportActionBar?.show()
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    supportActionBar?.setDisplayShowHomeEnabled(true)
+                }
+                else -> {
+                    supportActionBar?.show()
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                    supportActionBar?.setDisplayShowHomeEnabled(false)
+                }
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                Toast.makeText(this, "Settings ...", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        super.onPrepareOptionsMenu(menu)
+        val logoutItem = menu?.findItem(R.id.action_logout)
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+
 }
--- app/src/main/java/com/example/relojerialatam/view/Sucursal3/Sucursal3Fragment.kt
+++ app/src/main/java/com/example/relojerialatam/view/Sucursal3/Sucursal3Fragment.kt
@@ -5,56 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.relojerialatam.R
+import com.example.relojerialatam.databinding.FragmentSucursal2Binding
+import com.example.relojerialatam.databinding.FragmentSucursal3Binding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Sucursal3Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Sucursal3Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentSucursal3Binding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sucursal3, container, false)
+    ): View {
+        _binding =  FragmentSucursal3Binding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Sucursal3Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Sucursal3Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.sucursal3Direccion.setOnClickListener {
+            Toast.makeText(requireContext(), "Direccion Copiada", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
